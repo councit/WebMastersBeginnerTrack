ecff7f7d6e2aa6c14ec40c9202aed99f53693be2
Taylor Council <councit@gmail.com>
2020-09-19 02:01:37 -0700

    calculator project html and css complete

---
+++ JSIntro/app.js
@@ -0,0 +1,12 @@
+const monthlyRent = 500;
+
+const yearlyRent = monthlyRent * 12;
+console.log(yearlyRent);
+
+let myChar = 'T';
+
+let myRepeat = 10;
+
+for (let i = 1; i <= myRepeat; i++) {
+  console.log(myChar);
+}
